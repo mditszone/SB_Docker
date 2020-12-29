7a03568c0eaf23f2c58763d05b020ff48823867c
MDITS <mditszone@gmail.com>
2020-12-29 17:37:11 +0530

    update the project

---
+++ src/main/java/com/org/SpringBootDockerApplication.java
@@ -0,0 +1,21 @@
+package com.org;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootDockerApplication {
+
+	@GetMapping("/info")
+	public String getMessage() {
+		return " success.!!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerApplication.class, args);
+	}
+
+}
